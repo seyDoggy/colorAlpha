69a374f6c6b22a7892b849e6ae848f61d3e173f6
Adam Merrifield <macagp@gmail.com>
2013-05-28 17:10:28 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-colorAlpha
-==========
+# colorAlpha
+#### A jQuery plugin to set the alpha channel of a CSS color or background-color.
 
-A jQuery plugin to set the alpha channel of a CSS color or background-color.
+Sometimes you need to set the alpha channel of a color without setting the color itself. And no, opacity is not the same thing.
+
+### Usage
+
+```js
+$('#someElement').colorAlpha({
+	property : 'background-color', // accepts any css property that accepts a color (I think)
+	percent : 50 // where 50 = 50%
+});
+```
---
+++ src/jquery.coloralpha.js
@@ -0,0 +1,33 @@
+/**
+ * colorAlpha
+ * A jQuery plugin to set the alpha channel of a CSS color or background-color.
+ * 
+ * Latest version: https://github.com/seyDoggy/colorAlpha
+ * 
+ * License <https://github.com/seyDoggy/colorAlpha/blob/master/LICENSE.txt>
+ */
+ ;(function($, undefined) {
+	'use strict';
+
+	var pluginName = 'colorAlpha',
+		pluginVersion = '1.0.0';
+
+	$.fn.colorAlpha = function(options) {
+
+		/*
+		 * OPTIONS
+		 */
+		var settings = $.extend({
+			property:'background-color',
+			percent:'100'
+		}, options);
+
+		var match = /rgba?\((\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*(,\s*\d+[\.\d+]*)*\)/g.exec(this.css(settings.property));
+
+        settings.percent = settings.percent > 1 ? (settings.percent / 100) : settings.percent;
+
+        this.css(settings.property, "rgba(" + [match[1],match[2],match[3],settings.percent].join(',') +")");
+      
+		return this;
+	};
+})(jQuery);
